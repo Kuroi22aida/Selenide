ac51f5f07479f513a66e66979961a7d99aa8946e
Тарас Самойлов <kuroi22aida@gmail.com>
2022-08-03 15:54:52 +0400

    Rewrite my tests

--- src/test/java/ru/netology/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/CardDeliveryFormTest.java
@@ -5,52 +5,64 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryFormTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
     public void setup() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
-
     }
-
-    /*public Date appointedDate() {
-        Calendar calendar = new GregorianCalendar();
-        Date date = calendar.getTime();
-        return date;
-
-
-    }*/
-
-    /*public SimpleDateFormat meeting() {
-        SimpleDateFormat date = new SimpleDateFormat("dd-MM-yyyy");
-
-
-    }*/
-
 
     @Test
     void shouldOrderCardDelivery() {
 
+        String planningDate = generateDate(50);
 
         SelenideElement form = $(".form");
         $("[data-test-id=city] input").setValue("Саратов");
-        /*$("[data-test-id=date] input").setValue();*/
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Маркс Карл");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $("span.spin").shouldBe(Condition.visible);
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
+
+    }
+
+    @Test
+    void shouldErrorOrderCardDelivery() {
+
+        String planningDate = generateDate(1);
+
+        SelenideElement form = $(".form");
+        $("[data-test-id=city] input").setValue("Саратов");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Маркс Карл");
+        $("[data-test-id=phone] input").setValue("+70000000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id='date']")
+                .shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
+
 
     }
 }
